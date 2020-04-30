1a55c3b4d2d6f29ecc1884771363e9ad734e2467
Catie Tortorella <catietortorella@gmail.com>
2020-04-29 21:50:10 -0700

    added scss

--- StoreApp/ClientApp/src/components/NavBar.js
+++ StoreApp/ClientApp/src/components/NavBar.js
@@ -7,8 +7,13 @@ const NavBar = () => {
 
     return (
         <div className="nav-bar__container">
-            <NavLink to="/" activeClassName="selected"><FontAwesomeIcon icon={faHome} /></NavLink>
-            <div>
+            <div className="nav-bar__sec">
+                <NavLink to="/" activeClassName="selected">
+                    <div className="nav-bar__title">StoreApp</div>
+                </NavLink>
+            </div>
+
+            <div className="nav-bar__sec nav-bar__drawer">
                 <NavLink to="/add-item" activeClassName="selected">Create Item</NavLink>
                 <NavLink to="/max-price" activeClassName="selected">Find Max Price</NavLink>
             </div>
